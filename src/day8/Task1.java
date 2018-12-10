496ee28563b70aa108dba550c8ddd29dde8d6efd
Daniel Nilsson <daniel.nilsson94@outlook.com>
2018-12-10 22:01:44 +0100

    Solved day 8

---
+++ src/day8/Task1.java
@@ -0,0 +1,58 @@
+package day8;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Task1 {
+
+    public static void main(String[] args) throws IOException {
+        String absolutePath = new File(".").getAbsolutePath();
+        Queue<Integer> input = new ArrayDeque<>();
+        try (Scanner sc = new Scanner(Paths.get(absolutePath, "src/day8", "input.txt"))) {
+            while (sc.hasNext()) {
+                input.add(sc.nextInt());
+            }
+        }
+        Node root = getNode(input);
+        System.out.println(root.getMetadataSum());
+    }
+
+    private static Node getNode(Queue<Integer> input) {
+        int numberOfChildren = input.remove();
+        int numberOfMetadatas = input.remove();
+        Node n = new Node();
+        for (int i = 0; i < numberOfChildren; i++) {
+            n.children.add(getNode(input));
+        }
+        for (int i = 0; i < numberOfMetadatas; i++) {
+            n.metadatas.add(input.remove());
+        }
+        return n;
+    }
+
+    private static class Node {
+        private List<Node> children;
+        private List<Integer> metadatas;
+
+        public Node() {
+            this.children = new ArrayList<>();
+            this.metadatas = new ArrayList<>();
+        }
+
+        public int getMetadataSum() {
+            int metadataSum = metadatas.stream().mapToInt(Integer::valueOf).sum();
+            for (Node child : children) {
+                metadataSum += child.getMetadataSum();
+            }
+            return metadataSum;
+        }
+
+    }
+
+}
